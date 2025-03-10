3c68b8b86d6f1790d27d73e0b162bf329d4d10ca
Gabriel Ferreira Pereira <153655344+Ferreira502@users.noreply.github.com>
2025-03-10 07:30:37 -0300

    Update Exercicios01.c

--- EDs/ED01/Exercicios01.c
+++ EDs/ED01/Exercicios01.c
@@ -312,7 +312,7 @@ void metodo12 ( void )
 } // fim metodo12 ( )
 
 // identificar
-menuOpcoes ( void ) 
+void menuOpcoes ( void ) 
 {
    printf ( "\n" );             // para saltar linha
    printf ( "%s\n", "Exercicio01 - v.0.0 - 11/02/2025"       );
